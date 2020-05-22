eba705ec016af1838689f523615566fc4479d2c7
Jessie <jessie.griffin@icloud.com>
2020-05-21 22:22:07 -0400

    Adds model controller file

---
+++ Tips/Model Controller/LSITipController.h
@@ -0,0 +1,17 @@
+//
+//  LSITipController.h
+//  Tips
+//
+//  Created by Jessie Ann Griffin on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITipController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tips/Model Controller/LSITipController.m
@@ -0,0 +1,15 @@
+//
+//  LSITipController.m
+//  Tips
+//
+//  Created by Jessie Ann Griffin on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITipController.h"
+
+@implementation LSITipController
+
+
+
+@end
