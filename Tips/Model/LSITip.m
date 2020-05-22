88bca76f0c04c40ba40e0ea976449db7b896d155
Jessie <jessie.griffin@icloud.com>
2020-05-21 21:52:45 -0400

    Adds model file to the project

---
+++ Tips/Model/LSITip.m
@@ -0,0 +1,15 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by Jessie Ann Griffin on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+
+
+@end
